6ec4ff3b38db79b49beb041a686f55eb6e6c45fc
Nandan Patil <nandan311231@gmail.com>
2024-10-24 06:34:55 -0500

    removed code to allow continue walk around the maze even after collecting all items, to satisfy edge case.

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -51,14 +51,6 @@ bool isPathToFreedom(MazeCell* start, const std::string& moves) {
             case Item::WAND: hasWand = true; break;
             default: break;
         }
-
-        //instructions might say to not have early return like this.
-        //if you find all items then make illegal move it shoudl return false i think
-
-        // Check if all items have been collected
-        if (hasSpellbook && hasPotion && hasWand) {
-            return true; // Early exit if all items collected
-        }
     }
 
     // Return true only if all items have been collected by the end of the moves
